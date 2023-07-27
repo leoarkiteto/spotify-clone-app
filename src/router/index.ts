17cbbaccdbd613e98d7f3a4337c3a292fe574cee
Leo Franca <leoarkiteto@gmail.com>
2023-07-27 10:43:41 +0100

    :bulb: Comment all unused paths at router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    /*
     {
       path: '/',
       name: 'home',
@@ -17,7 +17,9 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
-  ]
-})
 
-export default router
+     */
+  ],
+});
+
+export default router;
