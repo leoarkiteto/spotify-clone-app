9fc8a19b1e1bae6ef64b91c267059776794da2db
Leo Franca <leoarkiteto@gmail.com>
2023-07-27 14:48:00 +0100

    :construction: (Components): Create HomeCard

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+interface Props {
+  image: string;
+  title: string;
+  subtitle: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <RouterLink to="library">
+    <div class="cursor-pointer rounded-md bg-[#111] p-4 hover:bg-[#252525]">
+      <img :alt="props.title" :src="props.image" class="rounded-md" />
+      <p class="pt-4 text-[17px] font-semibold text-white">{{ props.title }}</p>
+      <p class="pb-3 pt-1 text-[14px] text-gray-400">{{ props.subtitle }}</p>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped></style>
