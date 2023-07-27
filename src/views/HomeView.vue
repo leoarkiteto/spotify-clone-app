a0c5a863f534d22b936915ccdd033648588b7447
Leo Franca <leoarkiteto@gmail.com>
2023-07-27 14:48:28 +0100

    :construction: (Views): Create HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,125 @@
+<script lang="ts" setup>
+import HomeCard from "@/components/HomeCard.vue";
+</script>
+
+<template>
+  <div class="p-8">
+    <button
+      class="cursor-pointer text-2xl font-semibold text-white hover:underline"
+      type="button"
+    >
+      Podcasts to make you smarter
+    </button>
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+
+      <HomeCard
+        image="https://picsum.photos/id/27/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/89/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/12/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/75/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+    </div>
+  </div>
+
+  <div class="p-8">
+    <button
+      class="cursor-pointer text-2xl font-semibold text-white hover:underline"
+      type="button"
+      >Recommended radio</button
+    >
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+
+      <HomeCard
+        image="https://picsum.photos/id/27/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/89/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/12/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/75/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+    </div>
+  </div>
+
+  <div class="p-8">
+    <button
+      class="cursor-pointer text-2xl font-semibold text-white hover:underline"
+      type="button"
+      >Recommended songs</button
+    >
+
+    <div class="py-1.5"></div>
+
+    <div class="flex items-center">
+      <HomeCard
+        image="https://picsum.photos/id/30/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+
+      <HomeCard
+        image="https://picsum.photos/id/27/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/89/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/12/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+      <HomeCard
+        image="https://picsum.photos/id/75/300/300"
+        subtitle="Subtitle is here"
+        title="Title is here"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
