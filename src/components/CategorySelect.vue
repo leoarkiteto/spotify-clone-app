52afe18b7f40624a8e84c76d03dab6b2c4519095
Leo Franca <leoarkiteto@gmail.com>
2023-07-27 15:27:49 +0100

    :construction: (Components): Create CategorySelect component

---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import uniqolor from "uniqolor";
+
+interface Props {
+  category: string;
+  image: string;
+}
+
+const props = defineProps<Props>();
+const randColor = ref("");
+randColor.value = uniqolor.random();
+</script>
+
+<template>
+  <RouterLink to="library">
+    <div
+      v-if="randColor.color"
+      :style="`background-color: ${randColor.color}`"
+      class="relative aspect-square overflow-hidden rounded-xl brightness-[0.87] contrast-[2]"
+    >
+      <div class="pl-4 pt-4 text-[23px] font-semibold text-white">{{
+        props.category
+      }}</div>
+      <img
+        :alt="props.category"
+        :src="props.image || ''"
+        class="absolute -right-3.5 bottom-0 max-w-[100px] rotate-[20deg] contrast-[0.55]"
+      />
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped></style>
