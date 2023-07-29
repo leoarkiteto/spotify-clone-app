f21d29091a3a657b2f9ac15e33e0de6f2740f7c7
Leo Franca <leoarkiteto@gmail.com>
2023-07-29 12:26:23 +0100

    :recycle: Convete json to ts

---
+++ src/db/artist.ts
@@ -0,0 +1,67 @@
+export default {
+  name: "Poeira Pura",
+  albumCover: "/images/albumCovers/Roberto-Ribeiro-1977.jpg",
+  releaseYear: "1977",
+  tracks: [
+    {
+      id: 1,
+      name: "Liberdade",
+      path: "/songs/Liberdade.mp3",
+    },
+    {
+      id: 2,
+      name: "Coite, Cuia",
+      path: "/songs/Coité, Cuia.mp3",
+    },
+    {
+      id: 3,
+      name: "Um dia de Rei",
+      path: "/songs/Um Dia De Rei.mp3",
+    },
+    {
+      id: 4,
+      name: "Prece a Xangô",
+      path: "/songs/Prece a Xangô.mp3",
+    },
+    {
+      id: 5,
+      name: "Propagas",
+      path: "/songs/Propagas.mp3",
+    },
+    {
+      id: 6,
+      name: "Caro amor",
+      path: "/songs/Caro Amor.mp3",
+    },
+    {
+      id: 7,
+      name: "Engenho novo",
+      path: "/songs/Engenho Novo.mp3",
+    },
+    {
+      id: 8,
+      name: "Pernambuco, O leao do norte",
+      path: "/songs/Pernambuco o Leao do Norte.mp3",
+    },
+    {
+      id: 9,
+      name: "Amor de verdade",
+      path: "/songs/Amor De Verdade.mp3",
+    },
+    {
+      id: 10,
+      name: "Cabide de molambo",
+      path: "/songs/Cabide de Molambo.mp3",
+    },
+    {
+      id: 11,
+      name: "Inicio da felicidade",
+      path: "/songs/Início de Felicidade.mp3",
+    },
+    {
+      id: 12,
+      name: "Poeira Pura",
+      path: "/songs/Poeira Pura.mp3",
+    },
+  ],
+};
