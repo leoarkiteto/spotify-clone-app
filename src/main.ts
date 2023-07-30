5a536ab8fdb94beb3aa95689cc8d07c00080ed82
Leo Franca <leoarkiteto@gmail.com>
2023-07-30 10:11:38 +0100

    :heavy_plus_sign: Add Lottie package

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPlugintPersistedstate from "pinia-plugin-persistedstate";
+import Vue3Lottie from "vue3-lottie";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,5 +15,6 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(Vue3Lottie);
 
 app.mount("#app");
